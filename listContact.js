1d3a031dc6abde2e6afdbccf055841e2f2246975
Elio Vega <eliosilver@hotmail.com>
2023-07-23 21:50:09 -0500

    segundaentrega

--- listContact.js
+++ listContact.js
@@ -1,22 +1,46 @@
-let listContact = ["Elio Vega","Kelly Miranda","Sofia Ramirez","Natalia Miranda","Patricia Cuevas","Sergio Gavalo"];
+let listContact = [];
 
-function addContact(name){
-return listContact.push(name);
+
+function addContact(name, lastName, phone, city, address ){
+    const newContact = {
+        id: listContact.length + 1,
+        name,
+        lastName,
+        phone,
+        location:{
+            city,
+            address
+        }
+    }
+    return listContact.push(newContact);
 }
 
-/*addContact("Maicol Vega");
-console.log(listContact);*/
 
-function deleteContact(name){
+
+
+addContact("Elio", "Vega", 3192335588, "Bogota", "Cll 63 # 77a-50");
+addContact("Sofia", "Ramirez", 3205889655, "Bogota", "Crr 79 # 19-19")
+addContact("Patricia", "Cuevas", 3115692145, "Bogota", "Crr 111b # 101-20")
+
+console.log(listContact);
+
+
+
+function deleteContact(contact){
+    const datos = contact.split("");
+    const name = datos[0];
+    const lastName = datos [1];
     for(let i = 0; i < listContact.length; i++){
-        if(listContact[i] == name){
+        if(listContact[i].name === name && listContact[1].lastName === lastName){
             return listContact.splice(i, 1);
         }
     }
 }
 
-/*deleteContact("Sofia Ramirez");
-console.log(listContact);*/
+
+//deleteContact("Elio Vega");
+//console.log(listContact);//
+
 
 function printContact(){
     console.log("List Contact:"); 
@@ -24,5 +48,5 @@ function printContact(){
         console.log(listContact[i]);
 }
 
-//Imprimir la lista de contactos//
-printContact(listContact);
+
+//printContact(listContact);//
