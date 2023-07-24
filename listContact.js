c026be705861e10253fa6f45e1a12fb0eaa96a2f
Elio Vega <eliosilver@hotmail.com>
2023-07-23 22:21:06 -0500

    terceraentrega

--- listContact.js
+++ listContact.js
@@ -1,6 +1,5 @@
 let listContact = [];
 
-
 function addContact(name, lastName, phone, city, address ){
     const newContact = {
         id: listContact.length + 1,
@@ -22,24 +21,48 @@ addContact("Elio", "Vega", 3192335588, "Bogota", "Cll 63 # 77a-50");
 addContact("Sofia", "Ramirez", 3205889655, "Bogota", "Crr 79 # 19-19")
 addContact("Patricia", "Cuevas", 3115692145, "Bogota", "Crr 111b # 101-20")
 
+//console.log(listContact)//
+
+
+
+
+function findContact(contact) {
+    const [name, lastName] = contact.split(" ");
+    
+    const indiceContacto = listContact.findIndex(contactFilter => {
+      return contactFilter.name === name && contactFilter.lastName === lastName;
+    });
+    
+    return indiceContacto !== -1 ? indiceContacto : -1;
+  }
+
+
+
+  function deleteContact(contact) {
+    const indice = findContact(contact);
+    if (indice == -1){
+        return listContact;
+    }
+    return listContact.splice(indice, 1);
+}
+
+
+deleteContact("Elio Vega");
 console.log(listContact);
 
 
 
-function deleteContact(contact){
-    const datos = contact.split("");
-    const name = datos[0];
-    const lastName = datos [1];
-    for(let i = 0; i < listContact.length; i++){
-        if(listContact[i].name === name && listContact[1].lastName === lastName){
-            return listContact.splice(i, 1);
-        }
+
+
+function updateContact(contact, datos) {
+    const indice = findContact(contact);
+    if (indice == -1){
+        return listContact;
     }
+    listContact[indice] = { ...listContact[indice], ...datos };
 }
 
 
-//deleteContact("Elio Vega");
-//console.log(listContact);//
 
 
 function printContact(){
@@ -49,4 +72,4 @@ function printContact(){
 }
 
 
-//printContact(listContact);//
+printContact(listContact);
